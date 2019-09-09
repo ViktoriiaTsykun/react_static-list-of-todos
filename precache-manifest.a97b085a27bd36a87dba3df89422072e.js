78116fa8a837191946ceaa801e2c2ffaba2498cd
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-09-09 21:12:22 +0300

    Updates

---
+++ precache-manifest.a97b085a27bd36a87dba3df89422072e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cacac3c14bb493ea139d9a9a36197d05",
+    "url": "/react_static-list-of-todos/index.html"
+  },
+  {
+    "revision": "9c8704b63b0092a7455b",
+    "url": "/react_static-list-of-todos/static/css/main.3ef84f68.chunk.css"
+  },
+  {
+    "revision": "58ca6ceb802300dae504",
+    "url": "/react_static-list-of-todos/static/js/2.d97d6a68.chunk.js"
+  },
+  {
+    "revision": "9c8704b63b0092a7455b",
+    "url": "/react_static-list-of-todos/static/js/main.bc3c8672.chunk.js"
+  },
+  {
+    "revision": "ea6f31786c2aed313835",
+    "url": "/react_static-list-of-todos/static/js/runtime~main.00529491.js"
+  },
+  {
+    "revision": "8256ad05df049a512c23e70a9268386d",
+    "url": "/react_static-list-of-todos/static/media/Drawing-Pin.8256ad05.png"
+  },
+  {
+    "revision": "88c1fd2facedc0892968606fdf5a9c36",
+    "url": "/react_static-list-of-todos/static/media/background.88c1fd2f.jpg"
+  },
+  {
+    "revision": "9b81c298531cb0246720056713a02a9a",
+    "url": "/react_static-list-of-todos/static/media/mail.9b81c298.png"
+  },
+  {
+    "revision": "7bd46cff95f27a4e6f6fc60da31ceca3",
+    "url": "/react_static-list-of-todos/static/media/user.7bd46cff.png"
+  },
+  {
+    "revision": "c6569e20ade5a81d7e82ce39259a7ed4",
+    "url": "/react_static-list-of-todos/static/media/worldwide.c6569e20.png"
+  }
+]);
